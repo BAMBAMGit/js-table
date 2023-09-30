fbc742db3a3b51d9ba2147612c8e81a408622c23
George <gmittendorf@gmail.com>
2023-09-29 21:56:53 -0700

    datestring is wrong bc of UTC .. ugh

--- api/advance_who_up_row.js
+++ api/advance_who_up_row.js
@@ -28,7 +28,7 @@ const today = new Date();
 const year = today.getFullYear();
 const month = String(today.getMonth() + 1).padStart(2, '0'); // Months are 0-based
 const day = String(today.getDate()).padStart(2, '0');
-const dateString = String(year + '-' + month + '-' + day);
+const dateString = String(year + '-' + month + '-' + day-1);
 console.log('dateString')
 console.log(dateString)
 const dataRef = ref(database, dateString);
